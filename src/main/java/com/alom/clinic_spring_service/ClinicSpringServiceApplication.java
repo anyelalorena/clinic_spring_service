557b35619ff70b696d09764dbf5527a335179135
anyelalorena <anyelalorena@gmail.com>
2024-01-10 18:45:58 -0500

    [COD]Proyecto base springMVC

---
+++ src/main/java/com/alom/clinic_spring_service/ClinicSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alom.clinic_spring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alom/clinic_spring_service/ClinicSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alom.clinic_spring_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
